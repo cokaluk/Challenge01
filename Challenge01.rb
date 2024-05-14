e84969c1316c421dc3295658604c6b1a00b4d09d
cokaluk <cokaluk@academic.rrc.ca>
2024-05-14 09:00:35 -0500

    fixed pinky's ghost info

--- Challenge01.rb
+++ Challenge01.rb
@@ -9,7 +9,7 @@
 
 ghosts = [
  {:name => 'Inky', :age => 4, :loves => 'reindeers', :net_worth => 25},
- {:name => 'Pinky', :age => 5, :loves => 'garden', :net_worth => 14},
+ {:name => 'Pinky', :age => 5, :loves => 'garden tools', :net_worth => 14},
  {:name => 'Blinky', :age => 7, :loves => 'ninjas', :net_worth => 18.03},
  {:name => 'Clyde', :age => 6, :loves => 'yarn', :net_worth => 0}
 ]
